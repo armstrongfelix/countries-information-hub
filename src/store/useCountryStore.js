ca9f44fb5715d170ad0aa9243d20b50e2aea24e9
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-06-12 17:51:53 +0100

    modeide the api endpoint

--- src/store/useCountryStore.js
+++ src/store/useCountryStore.js
@@ -35,11 +35,14 @@ export const useCountryStore = create(
           if (offset <= 0) {
             set({ isLoading: true });
             offset += 100;
-            let res = await fetch(`${BASE_URL}&offset=${offset}`);
+            let res = await fetch(
+              `${BASE_URL}&offset=${offset}  { headers: { 'Authorization': 'Bearer rc_live_5fd9d335af0e42b29c0fb305b121579e' } `,
+            );
             if (!res.ok) {
               throw new Error(`Failed to fetch countries: ${res.status}`);
             }
             let json = await res.json();
+
             let data = json.data.objects;
 
             set({ currentFetch: data });
